524efa614236c62ce6fcb684478583f0a6e33936
Nathan Quartaro <nathanquartaro@Nathans-MacBook-Pro.local>
2023-03-02 11:40:47 -0500

    test

--- AppleTree/ProgramData/AppleManagement/SeedManagement/SeedManager.cs
+++
@@ -1,7 +0,0 @@
-namespace AppleTree.ProgramData.AppleManagement.SeedManagement;
-
-public class SeedManager // a seed = a file (Ex: helloWorld.py -> a seed)
-{
-    public string? LocalDir { get; set; }
-    public List<string>? TrackedFile { get; set; }
-}
---
+++ AppleTree/ProgramData/Utils/Constants.cs
@@ -0,0 +1,21 @@
+using AppleTree.ProgramData.TreeManagement;
+
+namespace AppleTree.ProgramData.Utils.Exceptions;
+
+public static class Constants
+{
+    internal static bool Exited;
+    internal static bool Debug;
+    internal static Tree? ActiveLocalTree;
+    internal static Settings? Settings;
+    
+    public static string? LocalTreeName
+    {
+        get
+        {
+            if (ActiveLocalTree != null)
+                return ActiveLocalTree.Name ?? null;
+            return null;
+        }
+    }
+}
